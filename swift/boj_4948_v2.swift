32f9969432deb2e14cf8469762fad0ad6bd0b803
hwangby <53309552+hwangby@users.noreply.github.com>
2019-08-06 20:32:16 +0900

    Update boj_4948_v2.swift

--- swift/boj_4948_v2.swift
+++ swift/boj_4948_v2.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019 HBY. All rights reserved.
 //
 
-import Foundation
+let n = 123456
+var input = readLine()!
+var check = Array(repeating: false, count: 2*n+1)
+for i in 2...n {
+    if !check[i] {
+        for j in stride(from: i*2, through: 2*n, by: i) {
+            check[j] = true // true : 소수가 아님
+        }
+    }
+}
+
+while input != "0" {
+    let num = Int(input)!
+    var count = 0
+    for i in stride(from: num+1, through: num*2, by: 1) {
+        if !check[i] {
+            count += 1
+        }
+    }
+    print(count)
+    input = readLine()!
+}
