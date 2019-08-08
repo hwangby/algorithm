85d33dd615f732cc0891cd260e02b6725d45fd8b
hwangby <53309552+hwangby@users.noreply.github.com>
2019-08-08 18:31:01 +0900

    Update boj_4153.swift

--- swift/boj_4153.swift
+++ swift/boj_4153.swift
@@ -1,13 +1,26 @@
+// while true {
+//     let sides = readLine()!.split(separator: " ").map{Int($0)!}
+//     if sides[0] == 0 {
+//         break
+//     }
+//     if sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2] {
+//         print("right")
+//     } else if sides[2]*sides[2] + sides[1]*sides[1] == sides[0]*sides[0] {
+//         print("right")
+//     } else if sides[2]*sides[2] + sides[0]*sides[0] == sides[1]*sides[1] {
+//         print("right")
+//     } else {
+//         print("wrong")
+//     }
+// }
+
+// 숫자 크기 순으로 재배열
 while true {
-    let sides = readLine()!.split(separator: " ").map{Int($0)!}
+    let sides = readLine()!.split(separator: " ").map{Int($0)!}.sorted() // 정렬
     if sides[0] == 0 {
         break
     }
     if sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2] {
-        print("right")
-    } else if sides[2]*sides[2] + sides[1]*sides[1] == sides[0]*sides[0] {
-        print("right")
-    } else if sides[2]*sides[2] + sides[0]*sides[0] == sides[1]*sides[1] {
         print("right")
     } else {
         print("wrong")
