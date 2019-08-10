7b19b2d38cb0a4c6341ee1ab57771ad56d5ed844
hwangby <53309552+hwangby@users.noreply.github.com>
2019-08-10 20:44:46 +0900

    Create boj_5086.swift

---
+++ swift/boj_5086.swift
@@ -0,0 +1,13 @@
+while true {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    if input == [0, 0] {
+        break
+    }
+    if input[1]%input[0] == 0 {
+        print("factor")
+    } else if input[0]%input[1] == 0 {
+        print("multiple")
+    } else {
+        print("neither")
+    }
+}
